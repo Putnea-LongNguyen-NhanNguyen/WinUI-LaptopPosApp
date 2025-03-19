7d7e61d09b714e65e302ee51ecd62eee5519e38d
Long Nguyen <nguyen.long.908132@gmail.com>
2025-03-19 15:04:35 +0700

    Add Entity Framework mock in-memory database

---
+++ LaptopPosApp/Dao/DbContextBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LaptopPosApp.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaptopPosApp.Dao
+{
+    public abstract class DbContextBase: DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder
+                .Entity<Category>()
+                .HasKey(e => e.ID);
+            modelBuilder
+                .Entity<Manufacturer>()
+                .HasKey(e => e.ID);
+            modelBuilder
+                .Entity<Product>()
+                .HasKey(e => e.ID);
+            modelBuilder
+                .Entity<Product>()
+                .HasOne(e => e.Category)
+                .WithMany(e => e.Products);
+            modelBuilder
+                .Entity<Product>()
+                .HasOne(e => e.Manufacturer)
+                .WithMany(e => e.Products);
+        }
+    }
+}
---
+++ LaptopPosApp/Dao/DbContextInMemoryMock.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Bogus;
+using LaptopPosApp.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaptopPosApp.Dao
+{
+    public partial class DbContextInMemoryMock : DbContextBase
+    {
+        private static List<Category> _seedCategories = new()
+        {
+            new ()
+            {
+                ID = 1,
+                Name = "Gaming"
+            },
+            new ()
+            {
+                ID = 2,
+                Name = "Văn phòng"
+            },
+            new()
+            {
+                ID = 3,
+                Name = "Workstation"
+            },
+        };
+        private static List<Manufacturer> _seedManufacturers = new()
+        {
+            new()
+            {
+                ID = 1,
+                Name = "Asus"
+            },
+            new()
+            {
+                ID = 2,
+                Name = "Acer"
+            },
+            new()
+            {
+                ID = 3,
+                Name = "Lenovo"
+            },
+            new()
+            {
+                ID = 4,
+                Name = "HP"
+            },
+            new()
+            {
+                ID = 5,
+                Name = "Dell"
+            },
+            new()
+            {
+                ID = 6,
+                Name = "MSI"
+            },
+            new()
+            {
+                ID = 7,
+                Name = "Apple"
+            },
+            new()
+            {
+                ID = 8,
+                Name = "Microsoft"
+            },
+            new()
+            {
+                ID = 9,
+                Name = "Corsair"
+            },
+            new()
+            {
+                ID = 10,
+                Name = "LG"
+            },
+            new()
+            {
+                ID = 11,
+                Name = "Google"
+            },
+        };
+        public DbContextInMemoryMock() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Data Source=InMemoryMockDb;Mode=Memory;Cache=Shared");
+            optionsBuilder.UseSeeding((context, _) =>
+            {
+                context.ChangeTracker.AutoDetectChangesEnabled = false;
+                context.Set<Category>().AddRange(_seedCategories);
+                context.Set<Manufacturer>().AddRange(_seedManufacturers);
+                var productGen = new Faker<Product>()
+                    .StrictMode(true)
+                    .RuleFor(o => o.ID, f => f.Random.String(15, 'A', 'Z'))
+                    .RuleFor(o => o.Name, f => f.Commerce.ProductName())
+                    .RuleFor(o => o.Description, f => f.Lorem.Sentences(2))
+                    .RuleFor(o => o.Price, f => (ulong)f.Finance.Amount(1000000, 20000000, 0))
+                    .RuleFor(o => o.Category, f => f.PickRandom(_seedCategories.AsEnumerable()))
+                    .RuleFor(o => o.Manufacturer, f => f.PickRandom(_seedManufacturers.AsEnumerable()));
+                context.Set<Product>().AddRange(productGen.GenerateBetween(10, 50));
+                context.ChangeTracker.AutoDetectChangesEnabled = true;
+                context.SaveChanges();
+            });
+        }
+    }
+}
