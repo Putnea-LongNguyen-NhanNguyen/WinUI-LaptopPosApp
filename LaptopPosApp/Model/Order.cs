a2206a4fa9b90d2cf06ddc5b4dbca5657e3fb695
Long Nguyen <nguyen.long.908132@gmail.com>
2025-03-22 23:08:31 +0700

    Add order related models (#7)
    * Add order related models
    
    * Remove old DAO classes

---
+++ LaptopPosApp/Model/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaptopPosApp.Model
+{
+    public class Order: IHasId
+    {
+        public required int ID { get; set; } = 0;
+        IComparable IHasId.ID => ID;
+        public required DateTime Timestamp { get; set; }
+        public required Customer Customer { get; set; }
+        public List<OrderProduct> Products { get; set; } = new();
+        public List<Voucher> Vouchers { get; set; } = new();
+        public required ulong TotalPrice { get; set; }
+    }
+}
---
+++ LaptopPosApp/Model/OrderProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaptopPosApp.Model
+{
+    public class OrderProduct
+    {
+        public required int OrderID { get; set; }
+        public required string ProductID { get; set; }
+        public required int Quantity { get; set; }
+        public Order Order { get; set; } = null!;
+        public Product Product { get; set; } = null!;
+    }
+}
