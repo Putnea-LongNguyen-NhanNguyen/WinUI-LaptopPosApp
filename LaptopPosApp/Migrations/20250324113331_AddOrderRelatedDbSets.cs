d3c77f0cc1a6978664d6924185d3eca4d615cd56
Long Nguyen <nguyen.long.908132@gmail.com>
2025-03-24 18:37:31 +0700

    Add order related DbSets (#11)

---
+++ LaptopPosApp/Migrations/20250324113331_AddOrderRelatedDbSets.cs
@@ -0,0 +1,202 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LaptopPosApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderRelatedDbSets : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_Customer_CustomerID",
+                table: "Order");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProduct_Order_OrderID",
+                table: "OrderProduct");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderVoucher_Order_OrdersID",
+                table: "OrderVoucher");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderVoucher_Voucher_VouchersCode",
+                table: "OrderVoucher");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Voucher",
+                table: "Voucher");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Order",
+                table: "Order");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Customer",
+                table: "Customer");
+
+            migrationBuilder.RenameTable(
+                name: "Voucher",
+                newName: "Vouchers");
+
+            migrationBuilder.RenameTable(
+                name: "Order",
+                newName: "Orders");
+
+            migrationBuilder.RenameTable(
+                name: "Customer",
+                newName: "Customers");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Order_CustomerID",
+                table: "Orders",
+                newName: "IX_Orders_CustomerID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Vouchers",
+                table: "Vouchers",
+                column: "Code");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders",
+                column: "ID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Customers",
+                table: "Customers",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProduct_Orders_OrderID",
+                table: "OrderProduct",
+                column: "OrderID",
+                principalTable: "Orders",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Customers_CustomerID",
+                table: "Orders",
+                column: "CustomerID",
+                principalTable: "Customers",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderVoucher_Orders_OrdersID",
+                table: "OrderVoucher",
+                column: "OrdersID",
+                principalTable: "Orders",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderVoucher_Vouchers_VouchersCode",
+                table: "OrderVoucher",
+                column: "VouchersCode",
+                principalTable: "Vouchers",
+                principalColumn: "Code",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProduct_Orders_OrderID",
+                table: "OrderProduct");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Customers_CustomerID",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderVoucher_Orders_OrdersID",
+                table: "OrderVoucher");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderVoucher_Vouchers_VouchersCode",
+                table: "OrderVoucher");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Vouchers",
+                table: "Vouchers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Customers",
+                table: "Customers");
+
+            migrationBuilder.RenameTable(
+                name: "Vouchers",
+                newName: "Voucher");
+
+            migrationBuilder.RenameTable(
+                name: "Orders",
+                newName: "Order");
+
+            migrationBuilder.RenameTable(
+                name: "Customers",
+                newName: "Customer");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_CustomerID",
+                table: "Order",
+                newName: "IX_Order_CustomerID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Voucher",
+                table: "Voucher",
+                column: "Code");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Order",
+                table: "Order",
+                column: "ID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Customer",
+                table: "Customer",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_Customer_CustomerID",
+                table: "Order",
+                column: "CustomerID",
+                principalTable: "Customer",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProduct_Order_OrderID",
+                table: "OrderProduct",
+                column: "OrderID",
+                principalTable: "Order",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderVoucher_Order_OrdersID",
+                table: "OrderVoucher",
+                column: "OrdersID",
+                principalTable: "Order",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderVoucher_Voucher_VouchersCode",
+                table: "OrderVoucher",
+                column: "VouchersCode",
+                principalTable: "Voucher",
+                principalColumn: "Code",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
