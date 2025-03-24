b6acfe6e09d94b4fa3ffbf941c25b95ee0926686
Nguyen Thanh Nhan <ntnhan221@clc.fitus.edu.vn>
2025-03-25 01:18:27 +0700

    files didn't get added

---
+++ LaptopPosApp/ViewModels/CategoriesRevenueViewModel.cs
@@ -0,0 +1,72 @@
+﻿using LaptopPosApp.Model;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView.Extensions;
+using LiveChartsCore.SkiaSharpView.Painting;
+using Microsoft.UI.Xaml;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaptopPosApp.ViewModels
+{
+    class CategoriesRevenueViewModel
+    {
+        private readonly List<Order> _orders;
+        public ObservableCollection<ISeries> GetSeries(DateTime startDate, DateTime endDate)
+        {
+            // i am stupid
+            Dictionary<Category, ulong> dict = [];
+            _orders
+                .Where(order => order.Timestamp >= startDate && order.Timestamp <= endDate)
+                .ToList()
+                .ForEach(order =>
+                {
+                    order.Products.ForEach(orderProduct =>
+                    {
+                        if (!dict.ContainsKey(orderProduct.Product.Category!))
+                        {
+                            dict[orderProduct.Product.Category!] = 0;
+                        }
+                        // get price in price history here
+                        // temporary price
+                        dict[orderProduct.Product.Category!] += orderProduct.Product.Price;
+                    });
+                });
+
+            // pie chart doesn't like ulong
+            List<string> categoryNames = [];
+            List<double> revenues = [];
+
+            foreach (var item in dict)
+            {
+                categoryNames.Add(item.Key.Name);
+                revenues.Add(item.Value);
+            }
+
+            int index = 0;
+
+            var result = new ObservableCollection<ISeries>(
+                revenues.AsPieSeries((value, series) =>
+                {
+                    series.Name = categoryNames[index++ % categoryNames.Count];
+                    series.DataLabelsPosition = LiveChartsCore.Measure.PolarLabelsPosition.Outer;
+                    series.DataLabelsSize = 15;
+                    series.DataLabelsFormatter = point => series.Name;
+                    series.ToolTipLabelFormatter = point => $"{point.StackedValue!.Share:P2}";
+                })
+            );
+
+            return result;
+        }
+
+        public CategoriesRevenueViewModel(List<Order> orders)
+        {
+            _orders = orders;
+        }
+    }
+}
---
+++ LaptopPosApp/ViewModels/ManufacturersRevenueViewModel.cs
@@ -0,0 +1,72 @@
+﻿using LaptopPosApp.Model;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView.Extensions;
+using LiveChartsCore.SkiaSharpView.Painting;
+using Microsoft.UI.Xaml;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaptopPosApp.ViewModels
+{
+    class ManufacturersRevenueViewModel
+    {
+        private readonly List<Order> _orders;
+        public ObservableCollection<ISeries> GetSeries(DateTime startDate, DateTime endDate)
+        {
+            // i am stupid
+            Dictionary<Manufacturer, ulong> dict = [];
+            _orders
+                .Where(order => order.Timestamp >= startDate && order.Timestamp <= endDate)
+                .ToList()
+                .ForEach(order =>
+                {
+                    order.Products.ForEach(orderProduct =>
+                    {
+                        if (!dict.ContainsKey(orderProduct.Product.Manufacturer!))
+                        {
+                            dict[orderProduct.Product.Manufacturer!] = 0;
+                        }
+                        // get price in price history here
+                        // temporary price
+                        dict[orderProduct.Product.Manufacturer!] += orderProduct.Product.Price;
+                    });
+                });
+
+            // pie chart doesn't like ulong
+            List<string> manufacturerNames = [];
+            List<double> revenues = [];
+
+            foreach (var item in dict)
+            {
+                manufacturerNames.Add(item.Key.Name);
+                revenues.Add(item.Value);
+            }
+
+            int index = 0;
+
+            var result = new ObservableCollection<ISeries>(
+                revenues.AsPieSeries((value, series) =>
+                {
+                    series.Name = manufacturerNames[index++ % manufacturerNames.Count];
+                    series.DataLabelsPosition = LiveChartsCore.Measure.PolarLabelsPosition.Outer;
+                    series.DataLabelsSize = 15;
+                    series.DataLabelsFormatter = point => series.Name;
+                    series.ToolTipLabelFormatter = point => $"{point.StackedValue!.Share:P2}";
+                })
+            );
+
+            return result;
+        }
+
+        public ManufacturersRevenueViewModel(List<Order> orders)
+        {
+            _orders = orders;
+        }
+    }
+}
