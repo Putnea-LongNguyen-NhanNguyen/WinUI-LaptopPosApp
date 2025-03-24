899aa2e43102e8cffae55492989f28108027aa00
Nguyen Thanh Nhan <ntnhan221@clc.fitus.edu.vn>
2025-03-24 17:47:47 +0700

    clean up revenue view model a bit

---
+++ LaptopPosApp/ViewModels/RevenueViewModel.cs
@@ -0,0 +1,181 @@
+﻿using LiveChartsCore.Defaults;
+using LiveChartsCore.Kernel.Sketches;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace LaptopPosApp.ViewModels
+{
+    class RevenueViewModel : INotifyPropertyChanged
+    {
+        private readonly List<DateTimePoint> _fakePoints;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ObservableCollection<ISeries> RevenueSeries =>
+            RevenueFilters[RevenueFilterIndex].FilterData(_fakePoints, StartDate.DateTime, EndDate.DateTime);
+
+        public string CurrentRevenueTimeSpanSum => SumInTimeSpan(_fakePoints, StartDate.DateTime, EndDate.DateTime).ToString("C");
+        private double SumInTimeSpan(List<DateTimePoint> data, DateTime startDate, DateTime endDate)
+        {
+            var sum = data.Where(dp => dp.DateTime >= startDate && dp.DateTime <= endDate)
+                .Sum(dp => dp.Value);
+            return sum != null ? (double)sum : 0;
+        }
+
+        public RevenueFilter CurrentRevenueFilter => RevenueFilters[RevenueFilterIndex];
+        public int SumTimeSpan 
+        { 
+            get; 
+            set
+            {
+                if (value >= 1)
+                    field = value;
+            }
+        } = 1;
+        public int RevenueFilterIndex { get; set; } = 2;
+        public List<RevenueFilter> RevenueFilters { get; set; }
+        public DateTimeOffset StartDate { get; set; } = new DateTimeOffset(DateTime.Today.AddDays(-700));
+        public DateTimeOffset EndDate { get; set; } = new DateTimeOffset(DateTime.Today);
+
+        private static List<DateTimePoint> GenerateFakePoints(DateTime startDate, DateTime endDate)
+        {
+            var random = new Random();
+            var data = new List<DateTimePoint>();
+            for (int i = 0; i <= (endDate - startDate).TotalDays; i++)
+            {
+                data.Add(new DateTimePoint(startDate.AddDays(i), random.Next(40000000, 80000000)));
+            }
+            return data;
+        }
+
+        public RevenueViewModel()
+        {
+            _fakePoints = GenerateFakePoints(StartDate.DateTime, EndDate.DateTime);
+            RevenueFilters = [
+                new DailyRevenueFilter() { Title = "Ngày", TimeSpan = 1 },
+                new WeeklyRevenueFilter() { Title = "Tuần", TimeSpan = 7 },
+                new MonthlyRevenueFilter() { Title = "Tháng", TimeSpan = 30 },
+            ];
+        }
+
+        public IEnumerable<ICartesianAxis> XAxes
+        {
+            get =>
+            [
+                new DateTimeAxis(
+                    TimeSpan.FromDays(RevenueFilters[RevenueFilterIndex].TimeSpan),
+                    date => date.ToString(RevenueFilters[RevenueFilterIndex].GetDateFormat())
+                )
+            ];
+        }
+
+        public IEnumerable<ICartesianAxis> YAxes
+        {
+            get =>
+            [
+                new Axis
+                {
+                    Labeler = (value) => value.ToString("C")
+                }
+            ];
+        }
+    }
+
+    abstract class RevenueFilter
+    {
+        public required string Title { get; set; }
+        public required int TimeSpan { get; set; }
+        public abstract string GetDateFormat();
+        public abstract ObservableCollection<ISeries> FilterData(List<DateTimePoint> data, DateTime startDate, DateTime endDate);
+    }
+
+    class MonthlyRevenueFilter : RevenueFilter
+    {
+        public MonthlyRevenueFilter()
+        {
+            Title = "Tháng";
+            TimeSpan = 30;
+        }
+        public override string GetDateFormat()
+        {
+            return "MM/yyyy";
+        }
+
+        override public ObservableCollection<ISeries> FilterData(List<DateTimePoint> data, DateTime startDate, DateTime endDate)
+        {
+            return [
+                new LineSeries<DateTimePoint>()
+                {
+                    Values = data.Where(dp => dp.DateTime >= startDate && dp.DateTime <= endDate)
+                    .GroupBy(dp => new {dp.DateTime.Year, dp.DateTime.Month})
+                    .Select(g => new DateTimePoint(new DateTime(g.Key.Year, g.Key.Month, 1), g.Sum(dp => dp.Value))),
+                    GeometrySize = 0,
+                }
+            ];
+        }
+    }
+
+    class WeeklyRevenueFilter : RevenueFilter
+    {
+        public WeeklyRevenueFilter()
+        {
+            Title = "Tuần";
+            TimeSpan = 7;
+        }
+        public override string GetDateFormat()
+        {
+            return "dd/MM/yyyy";
+        }
+
+        private static DateTime StartOfWeek(DateTime dt, DayOfWeek startOfWeek)
+        {
+            int diff = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
+            return dt.AddDays(-diff).Date;
+        }
+
+        override public ObservableCollection<ISeries> FilterData(List<DateTimePoint> data, DateTime startDate, DateTime endDate)
+        {
+            return [
+                new LineSeries<DateTimePoint>()
+                {
+                    Values = data.Where(dp => dp.DateTime >= startDate && dp.DateTime <= endDate)
+                    .GroupBy(dp => StartOfWeek(dp.DateTime, DayOfWeek.Monday))
+                    .Select(g => new DateTimePoint(g.Key, g.Sum(dp => dp.Value))),
+                    GeometrySize = 0,
+                }
+            ];
+        }
+    }
+
+    class DailyRevenueFilter : RevenueFilter
+    {
+        public DailyRevenueFilter()
+        {
+            Title = "Ngày";
+            TimeSpan = 1;
+        }
+
+        public override string GetDateFormat()
+        {
+            return "dd/MM/yyyy";
+        }
+
+        override public ObservableCollection<ISeries> FilterData(List<DateTimePoint> data, DateTime startDate, DateTime endDate)
+        {
+            return [
+                new LineSeries<DateTimePoint>()
+                {
+                    Values = data.Where(dp => dp.DateTime >= startDate && dp.DateTime <= endDate),
+                    GeometrySize = 0,
+                }
+            ];
+        }
+    }
+}
