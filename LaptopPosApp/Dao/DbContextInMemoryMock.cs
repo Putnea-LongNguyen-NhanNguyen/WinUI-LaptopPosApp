54d3e1ed2178aed1cfff1f9836d41abf0e451e61
Long Nguyen <nguyen.long.908132@gmail.com>
2025-03-26 17:52:36 +0700

    Fix mock data generation

--- LaptopPosApp/Dao/DbContextInMemoryMock.cs
+++ LaptopPosApp/Dao/DbContextInMemoryMock.cs
@@ -99,16 +99,15 @@ namespace LaptopPosApp.Dao
                 Categories.AddRange(_seedCategories);
                 Manufacturers.AddRange(_seedManufacturers);
                 var productGen = new Faker<Product>()
-                    .StrictMode(true)
                     .RuleFor(o => o.ID, f => f.Random.String(15, 'A', 'Z'))
                     .RuleFor(o => o.Name, f => f.Commerce.ProductName())
                     .RuleFor(o => o.Description, f => f.Lorem.Sentences(2))
                     .RuleFor(o => o.Price, f => (ulong)f.Finance.Amount(1000000, 20000000, 0))
-                    .RuleFor(o => o.Category, f => f.PickRandom(_seedCategories.AsEnumerable()))
-                    .RuleFor(o => o.Manufacturer, f => f.PickRandom(_seedManufacturers.AsEnumerable()))
-                    .RuleFor(o => o.Quantity, f => (ulong)f.Random.Int(1, 100))
-                    .RuleFor(o => o.TemporaryPrices, f => new List<ProductTemporaryPrice>());
-                Products.AddRange(productGen.GenerateBetween(10, 50));
+                    .RuleFor(o => o.Category, f => f.PickRandom(_seedCategories))
+                    .RuleFor(o => o.Manufacturer, f => f.PickRandom(_seedManufacturers))
+                    .RuleFor(o => o.Quantity, f => (ulong)f.Random.Int(1, 100));
+                var products = productGen.GenerateBetween(10, 50);
+                Products.AddRange(products);
 
                 var voucherFixedGen = new Faker<Voucher>()
                     .StrictMode(true)
@@ -119,7 +118,8 @@ namespace LaptopPosApp.Dao
                     .RuleFor(o => o.StartDate, f => f.Date.Recent(10))
                     .RuleFor(o => o.EndDate, f => f.Date.Future(90))
                     .RuleFor(o => o.Orders, f => new());
-                Vouchers.AddRange(voucherFixedGen.GenerateBetween(10, 50));
+                var vouchers = voucherFixedGen.GenerateBetween(10, 50);
+                Vouchers.AddRange(vouchers);
 
                 var customerGen = new Faker<Customer>()
                     .StrictMode(true)
@@ -129,36 +129,36 @@ namespace LaptopPosApp.Dao
                     .RuleFor(o => o.Phone, f => f.Phone.PhoneNumber())
                     .RuleFor(o => o.Email, f => f.Internet.Email())
                     .RuleFor(o => o.Orders, f => new());
-                Customers.AddRange(customerGen.GenerateBetween(10, 50));
+                var customers = customerGen.GenerateBetween(10, 50);
+                Customers.AddRange(customers);
 
                 var orderGen = new Faker<Order>()
                     .RuleFor(o => o.ID, f => 0)
                     .RuleFor(o => o.Timestamp, f => f.Date.Between(DateTime.Today.AddDays(-700), DateTime.Today))
-                    .RuleFor(o => o.Customer, f => f.PickRandom(Customers.Local.AsEnumerable()))
-                    .RuleFor(o => o.Vouchers, f => [f.PickRandom(Vouchers.Local.AsEnumerable())])
-                    .RuleFor(o => o.Vouchers, f => new());
+                    .RuleFor(o => o.Customer, f => f.PickRandom(customers))
+                    .RuleFor(o => o.Vouchers, f => [f.PickRandom(vouchers)]);
                 var orders = orderGen.GenerateBetween(10, 50);
                 orders.ForEach(o =>
                 {
                     Faker faker = new Faker();
-                    var product = faker.PickRandom(Products.Local.AsEnumerable());
-
-                    var orderProductGen = new Faker<OrderProduct>()
-                        .RuleFor(op => op.OrderID, f => o.ID)
-                        .RuleFor(op => op.ProductID, f => product.ID)
-                        .RuleFor(op => op.Quantity, f => f.Random.Int(1, 3))
-                        .RuleFor(op => op.Order, f => o)
-                        .RuleFor(op => op.Product, f => product);
-                    var orderProduct = orderProductGen.Generate();
-                    o.Products.Add(orderProduct);
-                    o.Vouchers.ForEach(v =>
+                    var productsOrdered = faker.PickRandom(products, 3);
+                    var orderProduct = productsOrdered.Select(p =>
                     {
-                        if (v.Type == VoucherType.Fixed)
-                            o.TotalPrice = product.Price - v.Quantity;
-                        else o.TotalPrice = product.Price * (ulong)((double)(100 - v.Quantity) / 100);
+                        return new OrderProduct()
+                        {
+                            OrderID = o.ID,
+                            ProductID = p.ID,
+                            Quantity = faker.Random.Int(1, 3),
+                            Order = o,
+                            Product = p
+                        };
                     });
-                    if (o.TotalPrice == 0)
-                        o.TotalPrice = product.Price;
+                    o.Products.AddRange(orderProduct);
+                    o.TotalPrice = o.Products.Aggregate(0ul, (acc, op) => acc + op.Product.Price * (ulong)op.Quantity);
+                    var reductions = o.Vouchers.Aggregate(0ul, (acc, v) =>
+                        acc + (v.Type == VoucherType.Fixed ? v.Value : (ulong)(o.TotalPrice * (v.Value / 100.0)))
+                    );
+                    o.TotalPrice = o.TotalPrice > reductions ? o.TotalPrice - reductions : 0;
                 });
                 Orders.AddRange(orders);
 
