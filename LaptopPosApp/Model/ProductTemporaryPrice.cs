8237470f360c9465ec9502ae13d51dcfe90f37ae
Long Nguyen <nguyen.long.908132@gmail.com>
2025-03-26 20:39:37 +0700

    Add INPC to `ProductTemporaryPrice`

--- LaptopPosApp/Model/ProductTemporaryPrice.cs
+++ LaptopPosApp/Model/ProductTemporaryPrice.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace LaptopPosApp.Model
 {
-    public class ProductTemporaryPrice
+    public partial class ProductTemporaryPrice: ObservableObject
     {
-        public required string ProductID { get; set; }
-        public required DateTime StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
-        public required ulong Price { get; set; }
+        [ObservableProperty]
+        public required partial string ProductID { get; set; }
+        [ObservableProperty]
+        public partial DateTime StartDate { get; set; }
+        [ObservableProperty]
+        public partial DateTime? EndDate { get; set; }
+        [ObservableProperty]
+        public required partial ulong Price { get; set; }
         public Product Product { get; set; } = null!;
     }
 }
