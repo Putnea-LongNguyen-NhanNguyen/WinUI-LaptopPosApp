a0379166026ee54e90c6965dceea297151398547
22127228 <lglac22@clc.fitus.edu.vn>
2025-04-13 22:40:45 +0700

    Add order detail window

---
+++ LaptopPosApp/Views/OrderDetailWindow.xaml.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+using LaptopPosApp.Model;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace LaptopPosApp.Views
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class OrderDetailWindow : Window
+    {
+        public static OrderDetailWindow? Instance { get; private set; }
+        public IEnumerable<OrderProduct> CurrentOrder { get; private set; }
+        private OrderDetailWindow(IEnumerable<OrderProduct> CurrentOrder)
+        {
+            this.InitializeComponent();
+            this.CurrentOrder = CurrentOrder;
+        }
+
+        public static OrderDetailWindow CreateInstance(IEnumerable<OrderProduct> currentOrder)
+        {
+            if (Instance != null)
+            {
+                Instance.Close();
+                Instance = null;
+            }
+
+            Instance = new OrderDetailWindow(currentOrder);
+            Instance.Closed += (s, e) => Instance = null;
+            return Instance;
+        }
+    }
+}
