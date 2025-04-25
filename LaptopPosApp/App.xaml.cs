8959ab6e4bd1fddd1d697269cd854bc94a658fda
Nguyen Thanh Nhan <ntnhan221@clc.fitus.edu.vn>
2025-04-25 23:04:13 +0700

    payment with momo

--- LaptopPosApp/App.xaml.cs
+++ LaptopPosApp/App.xaml.cs
@@ -1,20 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using LaptopPosApp.Views;
@@ -22,8 +7,8 @@ using LaptopPosApp.Dao;
 using LaptopPosApp.ViewModels;
 using Microsoft.EntityFrameworkCore;
 using LaptopPosApp.Services;
-using Microsoft.Extensions.Configuration;
-using LaptopPosApp.Services;
+using vietnam_qr_pay_csharp;
+using System.Diagnostics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
