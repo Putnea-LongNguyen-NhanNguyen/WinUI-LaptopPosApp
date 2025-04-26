135c0c0da691352014bb45f9b54d190ff55b7985
Long Nguyen <nguyen.long.908132@gmail.com>
2025-04-26 10:12:15 +0700

    Add return date to OrderProduct (#31)

---
+++ LaptopPosApp/Migrations/20250426031031_AddReturnDateToOrderProduct.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LaptopPosApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReturnDateToOrderProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ReturnDate",
+                table: "OrderProduct",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReturnDate",
+                table: "OrderProduct");
+        }
+    }
+}
